18e36d2796b4c63786d784fd746ee64fdfbc25e1
John Reese <john@noswap.com>
2018-06-11 20:22:30 -0700

    Fix manifest format

--- fissix/__init__.py
+++ fissix/__init__.py
@@ -13,7 +13,7 @@ from appdirs import user_cache_dir
 
 from .pgen2 import driver
 
-__version__ = "18.6a2"
+__version__ = "18.6a3"
 __base_version__ = "3.8.0a0"
 __base_revision__ = "v3.7.0a4-888-g4acc140f8d"
 
