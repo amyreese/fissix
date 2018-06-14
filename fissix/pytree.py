2b6b47f717199e0fb602e464014246d9a459535c
John Reese <john@noswap.com>
2018-06-14 18:16:32 +0000

    Make Base/Leaf/Node share type for .children

--- fissix/pytree.py
+++ fissix/pytree.py
@@ -47,7 +47,7 @@ class Base(object):
     # Default values for instance variables
     type = None  # int: token number (< 256) or symbol number (>= 256)
     parent = None  # Parent node pointer, or None
-    children = ()  # Tuple of subnodes
+    children = []  # List of subnodes
     was_changed = False
     was_checked = False
 
