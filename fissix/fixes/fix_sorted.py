5745683c3069cc08fde9aae6c2b41baf795577ef
Larry Huang <squirrel532@plurk.com>
2020-10-05 01:07:09 +0800

    lint

--- fissix/fixes/fix_sorted.py
+++ fissix/fixes/fix_sorted.py
@@ -39,11 +39,11 @@ class FixSorted(fixer_base.BaseFix):
         positional_args = list(
             filterfalse(lambda arg: arg.type in (token.COMMA, symbols.argument), nodes)
         )
-        template = ['cmp', 'key', 'reverse']
+        template = ["cmp", "key", "reverse"]
         new_args = []
         for arg, key in zip(positional_args, template):
             arg_ = arg.clone()
-            arg_.prefix = ''
+            arg_.prefix = ""
 
             new_arg = KeywordArg(Name(key), arg_)
             new_arg.prefix = arg.prefix
@@ -72,34 +72,34 @@ class FixSorted(fixer_base.BaseFix):
         return parent.children
 
     def _transform_cmp(self, nodes, result):
-        '''transform argument `cmp` into `key`'''
+        """transform argument `cmp` into `key`"""
         arglist = list(filter(lambda arg: arg.type == symbols.argument, nodes))
 
         cmp_node = None
         key_node = None
         for arg in arglist:
             if arg.type == symbols.argument:
-                if arg.children[0].value == 'cmp':
+                if arg.children[0].value == "cmp":
                     cmp_node = arg
-                if arg.children[0].value == 'key':
+                if arg.children[0].value == "key":
                     key_node = arg
 
         if cmp_node:
             if key_node:
                 return  # Do nothing when it have both cmp and key argument
 
-            cmp_node.children[0].value = 'key'
+            cmp_node.children[0].value = "key"
             cmp_node.children[2].replace(
-                Call(Name('cmp_to_key'), args=[cmp_node.children[2].clone()])
+                Call(Name("cmp_to_key"), args=[cmp_node.children[2].clone()])
             )
-            touch_import('functools', 'cmp_to_key', cmp_node)
+            touch_import("functools", "cmp_to_key", cmp_node)
 
     def transform(self, node, result):
-        if result.get('func_arg'):
-            a = Node(symbols.arglist, [r.clone() for r in result['func_arg']])
-            result['func_arg'][0].replace(a)
-            result['func_args'] = a.children
+        if result.get("func_arg"):
+            a = Node(symbols.arglist, [r.clone() for r in result["func_arg"]])
+            result["func_arg"][0].replace(a)
+            result["func_args"] = a.children
 
-        if result.get('func_args'):
-            ret = self._transform_keyword(result['func_args'], result)
+        if result.get("func_args"):
+            ret = self._transform_keyword(result["func_args"], result)
             self._transform_cmp(ret, result)
